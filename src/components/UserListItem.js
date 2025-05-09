c4dff159d2fcc48c2d310fa2fd5410897e0c399d
dangvu2408 <hiend6887@gmail.com>
2025-05-09 15:25:32 +0700

    27th commit (09052025) responsive list_subscribed_course in 1900px screen

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function UserListItem({ user }) {
     return (
         <tr>
-            <td><strong>{user.fullname}</strong></td>
+            <td style={{fontFamily: 'SFPro Bold'}}>{user.fullname}</td>
             <td>{user.username}</td>
             <td>{user.gender || "Chưa cập nhật"}</td>
             <td>{user.dob || "Chưa cập nhật"}</td>
-            <td className="user_role_td" style={{alignItems: 'center'}}>{user.role === 1 ? (
+            <td className="user_role_td" >{user.role === 1 ? (
                 <p className="admin_or_user">ADMIN</p>
             ) : (
                 <p className="user_or_admin">USER</p>
